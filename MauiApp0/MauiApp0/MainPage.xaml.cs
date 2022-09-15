ad1614ed9fbf3c844cde0c48a30971c19f4e8c1a
Shinya.Umeda <umeda@barnet.co.jp>
2022-09-15 12:12:41 +0900

    App0 ファイル読込, ポップアップ表示

--- MauiApp0/MauiApp0/MainPage.xaml.cs
+++ MauiApp0/MauiApp0/MainPage.xaml.cs
@@ -9,8 +9,48 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	//**********************************************************************************
+	private async void messageView()
+    {
+		await DisplayAlert("Alert", "You have been alerted", "OK");
+
+        bool answer = await DisplayAlert("Question?", "Would you like to play a game", "Yes", "No");
+        await DisplayAlert("Alert", answer.ToString(), "OK");
+
+        string action = await DisplayActionSheet("ActionSheet: Send to?", "Cancel", null, "Email", "Twitter", "Facebook");
+        await DisplayAlert("Alert", action, "OK");
+    }
+
+	private async Task<FileResult> ReadFile(PickOptions options)
 	{
+        try
+        {
+            var result = await FilePicker.Default.PickAsync(options);
+            if (result != null)
+            {
+                if (result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
+                    result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
+                {
+                    using var stream = await result.OpenReadAsync();
+                    var image = ImageSource.FromStream(() => stream);
+                }
+            }
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            // The user canceled or something went wrong
+        }
+
+        return null;
+    }
+
+
+
+	//**********************************************************************************
+	private void OnCounterClicked(object sender, EventArgs e)
+    {
 		count++;
 
 		if (count == 1)
@@ -20,5 +60,42 @@ public partial class MainPage : ContentPage
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
+
+	//**********************************************************************************
+	private async void Click_btnPopUp(object sender, EventArgs e)
+	{
+		//messageView();
+        await DisplayAlert("Alert", "You have been alerted", "OK");
+
+        bool answer = await DisplayAlert("Question?", "Would you like to play a game", "Yes", "No");
+        await DisplayAlert("Alert", answer.ToString(), "OK");
+
+        string action = await DisplayActionSheet("ActionSheet: Send to?", "Cancel", null, "Email", "Twitter", "Facebook");
+        await DisplayAlert("Alert", action, "OK");
+    }
+
+	//**********************************************************************************
+	private async void Click_btnFileRead(object sender, EventArgs e)
+    {
+        //var customFileType = new FilePickerFileType(
+        //        new Dictionary<DevicePlatform, IEnumerable<string>>
+        //        {
+        //            { DevicePlatform.iOS, new[] { "public.my.comic.extension" } }, // or general UTType values
+        //            { DevicePlatform.Android, new[] { "application/comics" } },
+        //            { DevicePlatform.WinUI, new[] { ".cbr", ".cbz" } },
+        //            { DevicePlatform.Tizen, new[] { "*/*" } },
+        //            { DevicePlatform.macOS, new[] { "cbr", "cbz" } }, // or general UTType values
+        //        });
+
+        //PickOptions options = new()
+        //{
+        //    PickerTitle = "Please select a comic file",
+        //    FileTypes = customFileType,
+        //};
+
+        var select_file = await ReadFile(null);
+        if(select_file != null)
+            await DisplayAlert("Select file", select_file.FullPath, "OK");
+    }
 }
 
