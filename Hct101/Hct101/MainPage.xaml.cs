859902556b85a41ae31bfbd00634d7478bc08fb1
Shinya.Umeda <umeda@barnet.co.jp>
2022-10-27 02:34:10 +0900

    no message

---
+++ Hct101/Hct101/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace Hct101;
+
+using System;
+using System.Net;
+using MauiCtrl;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+    WebClient wc = new WebClient();
+    HtmlCtrl HC = new HtmlCtrl();
+    ClsCommon common = new ClsCommon();
+
+    public MainPage()
+	{
+		InitializeComponent();
+
+        //System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        lblDebug.Text = DeviceInfo.Current.Platform.ToString();
+        if(DeviceInfo.Current.Platform == DevicePlatform.WinUI)
+        {
+            this.WidthRequest = 400;
+        }
+    }
+
+    void writeLog(string val)
+    {
+        lblValue.Text += val;
+        ToolCtrl TC = new ToolCtrl();
+        TC.scroll_ScrollView(svLog, 0, lblValue.Height, true);
+    }
+
+    //************************************************************************************
+    private void OnCounterClicked_btnTime(object sender, EventArgs e)
+    {
+        string val = HC.getHtml(wc, common.aryHtml[(int)enmHtml.time]);
+        writeLog($"Time : {val}\n");
+    }
+
+    //************************************************************************************
+    private void OnCounterClicked_btnTemp(object sender, EventArgs e)
+    {
+        string val = HC.getHtml(wc, common.aryHtml[(int)enmHtml.temp]);
+        writeLog($"Temp : {val}\n");
+    }
+
+    //************************************************************************************
+    private void OnCounterClicked_btnHumd(object sender, EventArgs e)
+    {
+        string val = HC.getHtml(wc, common.aryHtml[(int)enmHtml.humd]);
+        writeLog($"Humd : {val}\n");
+    }
+}
+
---
+++ Hct101/Hct101/common.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hct101
+{
+    public enum enmHtml
+    {
+        time,
+        temp,
+        humd,
+        all
+    };
+    //internal class clsCommon : ContentPage
+    public class ClsCommon
+    {
+        public string[] aryHtml =
+        {
+            "http://esp32.airport/get?sts=time",
+            "http://esp32.airport/get?sts=temperture",
+            "http://esp32.airport/get?sts=humidity",
+            "http://esp32.airport/get?sts=all"
+        };
+        
+
+    }
+}
