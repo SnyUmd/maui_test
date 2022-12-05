b4dc56fcb59743b700ce776e827477e332002fe5
Shinya.Umeda <umeda@barnet.co.jp>
2022-12-05 16:14:51 +0900

    Ringプロジェクト作成

---
+++ Ring00/Ring00/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Ring00;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Ring00/Ring00/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Ring00;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
