9b41b357deae67411a0fbee60241c10e2b48ea78
Shinya.Umeda <umeda@barnet.co.jp>
2023-01-27 10:31:17 +0900

    no message

---
+++ Hct101/Hct101/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿//using Android.Views;
+
+namespace Hct101;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+
+    }
+}
---
+++ Hct101/Hct101/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Hct101;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
