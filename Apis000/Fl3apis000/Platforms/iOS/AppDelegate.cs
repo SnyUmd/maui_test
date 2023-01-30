eeb49d2816b454d5ced8e235449e6f0e4c432650
Shinya.Umeda <umeda@barnet.co.jp>
2023-01-30 13:34:35 +0900

    no message

--- Apis000/Fl3apis000/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Fl3apis000;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
