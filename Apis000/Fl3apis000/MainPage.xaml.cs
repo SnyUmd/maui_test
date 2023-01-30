a9839d2151f2efa6c102e96a4c5580f26e1030d2
Shinya.Umeda <umeda@barnet.co.jp>
2023-01-30 13:34:11 +0900

    no message

--- Apis000/Fl3apis000/MainPage.xaml.cs
+++ Apis000/Fl3apis000/MainPage.xaml.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿//using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Mvc;
-//using Swashbuckle.AspNetCore.Annotations;
 
 namespace Fl3apis000;
 
@@ -13,9 +8,46 @@ namespace Fl3apis000;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
-	public MainPage()
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+    }
+
+
+    [Route("api/{controller}")]
+    [ApiController]
+    public class TotoController : ControllerBase
+    {
+        // Todoアイテムの初期データ。本来はデータベースなどから取得する。
+        private static List<TodoItem> items = new List<TodoItem>() {
+            new TodoItem() { Id = 1, Name = @"犬の散歩", IsDone = false, },
+            new TodoItem() { Id = 2, Name = @"買い物", IsDone = true, },
+            new TodoItem() { Id = 3, Name = @"本棚の修理", IsDone = false },
+        };
+
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll()
+            => items;
+
+        [HttpGet("{id}", Name = "Todo")]
+        public ActionResult<TodoItem> GetById(int id)
+        {
+            var item = items.Find(i => i.Id == id);
+            if (item == null)
+                return NotFound();
+            return item;
+        }
+    }
+
+    //*********************************************************************************
+    //*********************************************************************************
+    //*********************************************************************************
+
+
+    public MainPage()
 	{
 		InitializeComponent();
 	}
