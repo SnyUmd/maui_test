116c3a236a2fc0d89539c59e98b617063a1049a5
DESKTOP-AC\isb <SnyUmd>
2023-03-15 09:51:52 +0900

    no message

---
+++ Call/Call/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Android.Content.Res;
+
+namespace Call;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		//MainPage = new AppShell();
+		
+		MainPage = new NavigationPage(new MainPage());
+
+        //await Navigation.PushModalAsync(modalArrivalInspection);//???
+
+
+    }
+}
---
+++ Call/Call/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Call;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
