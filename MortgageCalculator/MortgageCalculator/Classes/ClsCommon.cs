67d30e70e2b7cdf4f7d66e9c831266b5d2bdd922
UM_ENVY\umd <sny.umd@gmail.com>
2023-03-16 23:02:31 +0900

    no message

--- MortgageCalculator/MortgageCalculator/Classes/ClsCommon.cs
+++ MortgageCalculator/MortgageCalculator/Classes/ClsCommon.cs
@@ -18,13 +18,40 @@ namespace MortgageCalculator.Classes
         // Androidの場合
         public static readonly string LOCAL_APP_DATA = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 #endif
-        //#endif
+        public struct Status
+        {
+            public int LoanPrice;       //借入価格
+            public int InterestRate;    //金利
+            public int YearsOfRepayment;//返済年数
+            public int RepaymentType;   //返済タイプ
+            public int Saving;          //貯金
+            public int AgeA;
+            public int AgeB;
+            public int AgeC;
+
+            public Status()
+            {
+                LoanPrice = 0;
+                InterestRate = 0;
+                YearsOfRepayment = 0;
+                RepaymentType = 0;
+                Saving = 0;          //貯金
+                AgeA = 0;
+                AgeB = 0;
+                AgeC = 0;
+            }
+        }
+        public static Status LoanStatus = new();
 
         public static string[] ImageDataSources =
         {
             "equal_principal_and_interest.png",
             "equal_amount_of_principal.png"
         };
+
+        public void Init()
+        {
+        }
 
     }
 
--- MortgageCalculator/MortgageCalculator/MainPage.xaml.cs
+++ MortgageCalculator/MortgageCalculator/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MortgageCalculator.Classes;
 using System.Text.RegularExpressions;
 using MauiCtrl;
+using System.Diagnostics;
 
 namespace MortgageCalculator;
 
@@ -13,30 +14,36 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 		//this.WidthRequest = 500;
 		ClsDebug.DebugWriteLine(ClsCommon.CURRENT_DIRECTRY);
+
+        //標準ナビバー非表示
+        NavigationPage.SetHasNavigationBar(this, false);
 #if ANDROID
         ClsDebug.DebugWriteLine(ClsCommon.LOCAL_APP_DATA);
 #endif
-        SetPickerItems();
+
         Frame0.HeightRequest = Grid0.Height;
         Frame1.HeightRequest = Grid1.Height;
         Frame2.HeightRequest = Grid2.Height;
 
+        SetInit();
     }
 
-	//*******************************************************************
-	private void OnCounterClicked(object sender, EventArgs e)
+    //*******************************************************************
+    private void ClickedBtnCal(object sender, EventArgs e)
     {
-		count++;
+        ClsCommon.LoanStatus = GetLoanStatus();
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        Debug.WriteLine($"借入額：{ClsCommon.LoanStatus.LoanPrice} 万円");
+        Debug.WriteLine($"金利：{ClsCommon.LoanStatus.InterestRate} %");
+        Debug.WriteLine($"返済変数：{ClsCommon.LoanStatus.YearsOfRepayment} 年");
+        Debug.WriteLine($"返済タイプ：{ClsCommon.LoanStatus.RepaymentType}");
+        Debug.WriteLine($"貯金月額：{ClsCommon.LoanStatus.Saving} 万円 / 月");
+        Debug.WriteLine($"年齢A：{ClsCommon.LoanStatus.AgeA} 歳");
+        Debug.WriteLine($"年齢B：{ClsCommon.LoanStatus.AgeB} 歳");
+        Debug.WriteLine($"年齢C：{ClsCommon.LoanStatus.AgeC} 歳");
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
-
-	//*******************************************************************
+    //*******************************************************************
     /// <summary>
     /// 入力イベント　入力制限
     /// </summary>
@@ -61,19 +68,49 @@ public partial class MainPage : ContentPage
     }
 
     //*******************************************************************
-    private void SetPickerItems()
+    private void SetInit()
     {
+        //ピッカーの選択項目をセット
         var listLoanTypes = new List<string>();
         listLoanTypes.Add("元金均等返済");
         listLoanTypes.Add("元利均等返済");
         PickerType.ItemsSource = listLoanTypes;
         PickerType.SelectedIndex = 0;
         selectTypeImage(PickerType.SelectedIndex);
+
+        //umd仕様
+        EntryLoanPrice.Text = "4000";
+        EntryInterestRate.Text = "1";
+        EntryYearsOfRepayment.Text = "30";
+        PickerType.SelectedIndex = 0;
+        EntrySaving.Text = "10";
+        EntryAgeA.Text = "42";
+        EntryAgeB.Text = "51";
+        EntryAgeC.Text = "";
     }
 
+    //*******************************************************************
     private void selectTypeImage(int num)
     {
         ImageType.Source = ClsCommon.ImageDataSources[num];
     }
+
+    //*******************************************************************
+    private ClsCommon.Status GetLoanStatus()
+    {
+        ClsCommon.Status resultStatus = new();
+
+        resultStatus.LoanPrice = int.Parse(EntryLoanPrice.Text);
+        resultStatus.InterestRate = int.Parse(EntryInterestRate.Text);
+        resultStatus.YearsOfRepayment = int.Parse(EntryYearsOfRepayment.Text);
+        resultStatus.RepaymentType = PickerType.SelectedIndex;
+        resultStatus.Saving = int.Parse(EntrySaving.Text);
+        resultStatus.AgeA = int.Parse(EntryAgeA.Text);
+        resultStatus.AgeB = int.Parse(EntryAgeB.Text);
+        resultStatus.AgeC = int.Parse(EntryAgeC.Text);
+        
+        return resultStatus;
+    }
+
 }
 
